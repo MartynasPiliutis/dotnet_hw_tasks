575a120e268103cc6c33b02cca8d0f2af9254471
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-26 21:48:41 +0200

    hw task3 complete

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liekana = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i%3 == liekana)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
