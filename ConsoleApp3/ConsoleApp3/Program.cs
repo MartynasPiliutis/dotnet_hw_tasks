1ce35208cd13ff16fb6c142ebff0b926a5024822
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-26 21:41:37 +0200

    hw task3 complete

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int i = 10; i <= 20; i++)
+            {
+                suma = suma + i;
+                Console.WriteLine(suma);
+            }
+            Console.ReadLine();
+        }
+    }
+}
