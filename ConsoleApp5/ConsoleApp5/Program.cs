de77cb07738215fd00e0e3f7127479bcbd9f42ae
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-26 21:58:16 +0200

    HW task5 complete

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Iveskite eiluciu skaiciu: ");
+            int eilutes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= eilutes-1; i++)
+            {
+                for (int x = 0; x < i+1; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
