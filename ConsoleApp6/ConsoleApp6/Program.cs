9696e2385fc7e7a92fe9b5af521a564bd0ac36c1
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-26 22:03:43 +0200

    HW task6 complete

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int x = 1; x <= 10; x++)
+                {
+                    Console.Write("{0} x {1} = {2}, ", i, x, i * x);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
