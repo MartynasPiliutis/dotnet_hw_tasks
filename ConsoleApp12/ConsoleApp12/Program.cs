3222641fd6a62d6af2ec0a9d6bf65499ae44e614
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-27 21:50:00 +0200

    HW masyvai task7 + task8 complete

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masyvas = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                masyvas[i] = rnd.Next(1,100);
+            }
+            foreach (var item in masyvas)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+            Array.Sort(masyvas);
+
+            foreach (var item in masyvas)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
