a7e49696384faf1bb30cab733e6a53c94fa31af2
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-27 16:57:48 +0200

    hw masyvai task1 complete

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masyvas = new int[5];
+
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                Console.Write("Iveskite skaiciu: ");
+                masyvas[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            
+
+            Console.WriteLine("Jusu suvestas masyvas yra:");
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                Console.WriteLine(masyvas[i]);
+            }
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
