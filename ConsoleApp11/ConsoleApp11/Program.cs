aed31de5515c5b80c1ed246f3f48c24f7fa540b1
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-27 18:51:23 +0200

    hw masyvai task4 + a+b complete

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] tekstas = new char[8];
+            for (int i = 0; i < tekstas.Length; i++)
+            {
+                Console.Write("Iveskite raide: ");
+                tekstas[i] = Console.ReadKey().KeyChar;
+                Console.Clear();
+            }
+
+            for (int i = 0; i < tekstas.Length; i++)
+            {
+                Console.Write(tekstas[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            for (int x = tekstas.Length - 1; x >= 0; x--)
+            {
+                Console.Write(tekstas[x]);
+                Console.Write(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
